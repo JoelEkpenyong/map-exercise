98bbc9aa9847837532d489c623bae759faa4edd6
JoelEkpenyong <joelekpenyong@gmail.com>
2021-12-08 13:57:52 +0100

    #update: map and popup added

--- assets/script/app.js
+++ assets/script/app.js
@@ -7,24 +7,68 @@ let COUNTRY_DATA = [];
 let EXCHANGE_RATE = {};
 let SELECTEDCOUNTRY = {};
 
+const popupHtml = '';
+
+const map = L.map('map');
+
+L.tileLayer(
+  'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken:
+      'pk.eyJ1Ijoiam9lbGVrcGVueW9uZyIsImEiOiJja3d4Znd3bXMwZGF0MnFycm81eXI0b2MwIn0.EauZWfHlAP7Qa-3bDc4N5Q',
+  }
+).addTo(map);
+
+const marker = L.marker([1.0, 38.0]).addTo(map);
+
 const getCountries = () => {
   loadingWrapper.classList.add('loading');
-  fetch(
-    'https://restcountries.com/v3.1/all/?fields=name,currencies,capital,population'
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      COUNTRY_DATA = data;
-      return data.map((country) => country.name.common);
-    })
-    .then((countryNames) => {
-      appendToPage(countryNames);
-      loadingWrapper.classList.remove('loading');
-    })
-    .catch((err) => {
-      console.log('An error occured while getting countries: %s', err.message);
-      loadingWrapper.classList.remove('loading');
-    });
+  return new Promise((resolve, reject) => {
+    fetch(
+      'https://restcountries.com/v3.1/all/?fields=name,currencies,capital,population,capitalInfo'
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        COUNTRY_DATA = data;
+        return data.map((country) => country.name.common);
+      })
+      .then((countryNames) => {
+        appendToPage(countryNames);
+        loadingWrapper.classList.remove('loading');
+        resolve('Success');
+      })
+      .catch((err) => {
+        console.log(
+          'An error occured while getting countries: %s',
+          err.message
+        );
+        loadingWrapper.classList.remove('loading');
+        reject(err);
+      });
+  });
+};
+
+const getSelectedCountry = (name) =>
+  COUNTRY_DATA.find((country) => country.name.common === name);
+
+const formatSelectedCountry = (country, rate, weatherData) => {
+  const currencyCode = Object.keys(country.currencies)[0];
+
+  return {
+    name: country.name.common,
+    capital: country.capital[0],
+    population: country.population,
+    currency: `${country.currencies[currencyCode].name} (${currencyCode})`,
+    exchangeRate: `1 ${currencyCode} = ${rate} ${EXCHANGE_RATE.base}`,
+    currentWeather: weatherData,
+    latlng: country.capitalInfo['latlng'],
+  };
 };
 
 const appendToPage = (data) => {
@@ -51,6 +95,30 @@ const updateButtonText = (el) => {
   dropdownButton.textContent = el;
 };
 
+const showOnMap = ([lat, lng], content) => {
+  marker.setLatLng([lat, lng]);
+  marker
+    .bindPopup(
+      `<div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 id="countryName" class="card-title fs-5">${content?.name}</h5>
+        <h6 id="capitalCity" class="card-subtitle mb-2 text-muted fs-6 ">${content?.capital}</h6>
+        <p class="card-text fs-6">${content?.name} has a total population of ${content?.population}. It's currency is ${content?.currency} - ${content?.exchangeRate}</p>
+        <p class="card-text fs-6">
+          <span class=" fs-5 card-title">Current Weather</span> <br>
+          <span class="fs-6 mb-3 d-block text-capitalize">${content.currentWeather.weather[0].description}</span>
+          Temperature of ${content.currentWeather.main.temp}°C, humidity of ${content.currentWeather.main.humidity}%, and wind speeds of ${content.currentWeather.wind.speed}m/s
+        </p>
+
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+      </div>
+  	</div>`
+    )
+    .openPopup();
+  map.setView([lat, lng], 8);
+};
+
 const getCurrentLocation = async () => {
   if (window.navigator.geolocation) {
     return new Promise((resolve, reject) => {
@@ -60,7 +128,13 @@ const getCurrentLocation = async () => {
             `https://api.opencagedata.com/geocode/v1/json?q=${pos.coords.latitude}+${pos.coords.longitude}&key=9a427b614bb5400eb3c625a0836a58ff`
           );
           const data = await res.json();
-          resolve(data.results[0].components.country);
+          let countryName = data.results[0].components.country;
+          const country = getSelectedCountry(countryName);
+          const rate = await getCountryExchangeRate(country);
+          const weatherData = await getCityWeather(country);
+          const countryInfo = formatSelectedCountry(country, rate, weatherData);
+          showOnMap([pos.coords.latitude, pos.coords.longitude], countryInfo);
+          resolve(countryName);
         },
         (err) => {
           reject(err);
@@ -76,7 +150,7 @@ const getCityWeather = async (country) => {
   return new Promise(async (resolve, reject) => {
     try {
       const res = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=70e19ba461fd1eb09a6eea1bbf30338f`
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=70e19ba461fd1eb09a6eea1bbf30338f`
       );
       const data = res.json();
       resolve(data);
@@ -87,11 +161,16 @@ const getCityWeather = async (country) => {
 };
 
 const fetchExchangeRates = async (currency) => {
-  const res = await fetch(
-    'https://openexchangerates.org/api/latest.json?app_id=c61cb86d27c846648c2759cae6c10f72'
-  );
-  const data = await res.json();
-  EXCHANGE_RATE = { base: data.base, rates: data.rates };
+  return new Promise(async (resolve, reject) => {
+    const res = await fetch(
+      'https://openexchangerates.org/api/latest.json?app_id=c61cb86d27c846648c2759cae6c10f72'
+    );
+    const data = await res.json();
+    EXCHANGE_RATE = { base: data.base, rates: data.rates };
+
+    resolve(data);
+    reject('Failed');
+  });
 };
 
 const getCountryExchangeRate = (country) => {
@@ -102,24 +181,14 @@ const getCountryExchangeRate = (country) => {
 const handleClick = async (el) => {
   console.log('%s has been selected', el.target.textContent);
   loadingWrapper.classList.add('loading');
-  const country = COUNTRY_DATA.find(
-    (country) => country.name.common === el.target.textContent
-  );
+  const country = getSelectedCountry(el.target.textContent);
   try {
     const rate = await getCountryExchangeRate(country);
     const weatherData = await getCityWeather(country);
-    const currencyCode = Object.keys(country.currencies)[0];
-
-    const countriesInfo = {
-      name: country.name.common,
-      capital: country.capital[0],
-      population: country.population,
-      currency: `${country.currencies[currencyCode].name} (${currencyCode})`,
-      exchangeRate: `1 ${currencyCode} = ${rate} ${EXCHANGE_RATE.base}`,
-      currentWeather: weatherData,
-    };
+    const countriesInfo = formatSelectedCountry(country, rate, weatherData);
 
     SELECTEDCOUNTRY = countriesInfo;
+    showOnMap(country.capitalInfo['latlng'], countriesInfo);
     console.dir(SELECTEDCOUNTRY);
     loadingWrapper.classList.remove('loading');
   } catch (error) {
@@ -128,10 +197,10 @@ const handleClick = async (el) => {
 };
 
 const init = async () => {
-  getCountries();
+  await getCountries();
+  await fetchExchangeRates();
   const countryName = await getCurrentLocation();
   updateButtonText(countryName);
-  fetchExchangeRates();
 };
 
 window.onload = () => {
